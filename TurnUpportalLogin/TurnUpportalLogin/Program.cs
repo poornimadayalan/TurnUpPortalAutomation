8edcc4f342ee9c8201dc42b9096f83b8a86495ba
poornima dayalan <poornimadayalan@Sureshs-MacBook-Pro.local>
2023-12-28 12:07:09 +1300

    Automate login functionality

---
+++ TurnUpportalLogin/TurnUpportalLogin/Program.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//Open the Chrome browser
+
+IWebDriver driver = new ChromeDriver();
+
+//Launch TurnUp portal and navigate to website login page
+
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+//Identify username textbox and enter valid username
+
+IWebElement usernameTextBox = driver.FindElement(By.Id("UserName"));
+usernameTextBox.SendKeys("hari");
+
+//Identify password textbox and enter valid password
+
+IWebElement passwordTextBox = driver.FindElement(By.Id("Password"));
+passwordTextBox.SendKeys("123123");
+
+
+//Identify the login button and click on the button
+
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+loginButton.Click();
+
+//Check if user has logged in successfully
+
+IWebElement hellohari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+
+if (hellohari.Text == "Hello hari!")
+{
+    System.Console.WriteLine("User is correctly logged in!");
+}
+else
+{
+    System.Console.WriteLine("Login failed. Please try again.");
+}
