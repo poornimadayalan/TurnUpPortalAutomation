52504a9499630d1a6b6f24c63fa69fd95502dfb1
Poornima Suresh <poornimadayalan73@gmail.com>
2024-03-12 22:30:34 +1300

    This is the commit for Nunit Create Edit and Delete Modules

--- TurnUpPortalAutomation/Pages/TimeandMaterialPage.cs
+++ TurnUpPortalAutomation/Pages/TimeandMaterialPage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 using OpenQA.Selenium;
 
 namespace TurnUpPortalAutomation.Pages
@@ -50,75 +51,64 @@ namespace TurnUpPortalAutomation.Pages
             selectLastPage.Click();
 
             IWebElement savedData = driver.FindElement(By.XPath("/html/body/div[4]/div/div/div[3]/table/tbody/tr[last()]/td[1]"));
-            if (savedData.Text == "Dya Automation 24")
-            {
-            Console.WriteLine("Data saved in Time and Materials");
 
-            }
-             else
-             {
-            Console.WriteLine("Data not saved in Time and Materials");
-             }
+            Assert.That(savedData.Text.Equals("Dya Automation 24"), "Data not saved in Time and Materials");
         }
+        public void EditTimeRecord(IWebDriver driver)
 
-             public void EditTimeRecord(IWebDriver driver)
+        {
+            //Edit data
 
-             {
-             //Edit data
+            IWebElement clickOnEditButton = driver.FindElement(By.XPath("/html/body/div[4]/div/div/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
+            clickOnEditButton.Click();
 
-                IWebElement clickOnEditButton = driver.FindElement(By.XPath("/html/body/div[4]/div/div/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
-                clickOnEditButton.Click();
-    
-                IWebElement editCodeType = driver.FindElement(By.XPath("/html/body/div[4]/form/div/div[1]/div/span[1]/span/span[2]/span"));
-                editCodeType.Click();
-                Thread.Sleep(1000);
+            IWebElement editCodeType = driver.FindElement(By.XPath("/html/body/div[4]/form/div/div[1]/div/span[1]/span/span[2]/span"));
+            editCodeType.Click();
+            Thread.Sleep(1000);
 
-                IWebElement selectDropDown = driver.FindElement(By.XPath("/html/body/div[4]/form/div/div[1]/div/span[1]/span/span[1]"));
-                selectDropDown.Click();
-                Thread.Sleep(1000);
+            IWebElement selectDropDown = driver.FindElement(By.XPath("/html/body/div[4]/form/div/div[1]/div/span[1]/span/span[1]"));
+            selectDropDown.Click();
+            Thread.Sleep(1000);
 
-                IWebElement editCode = driver.FindElement(By.Id("Code"));
-                editCode.Clear();
-                editCode.SendKeys("Dya123");
-                Thread.Sleep(1000);
+            IWebElement editCode = driver.FindElement(By.Id("Code"));
+            editCode.Clear();
+            editCode.SendKeys("Dya123");
+            Thread.Sleep(1000);
 
-                IWebElement editDescription = driver.FindElement(By.Name("Description"));
-                editDescription.Clear();
-                editDescription.SendKeys("Edited Auto Code");
-                Thread.Sleep(1000);
+            IWebElement editDescription = driver.FindElement(By.Name("Description"));
+            editDescription.Clear();
+            editDescription.SendKeys("Edited Auto Code");
+            Thread.Sleep(1000);
 
-                IWebElement editPrice = driver.FindElement(By.XPath("/html[1]/body[1]/div[4]/form[1]/div[1]/div[4]/div[1]/span[1]/span[1]/input[1]"));
-                Console.WriteLine("Trying to edit in Time and Materials" + editPrice);
-                //editPrice.Clear();
-                editPrice.SendKeys("15");
-                Thread.Sleep(2000);
+            IWebElement editPrice = driver.FindElement(By.XPath("/html[1]/body[1]/div[4]/form[1]/div[1]/div[4]/div[1]/span[1]/span[1]/input[1]"));
+            Console.WriteLine("Trying to edit in Time and Materials" + editPrice);
+            //editPrice.Clear();
+            editPrice.SendKeys("15");
+            Thread.Sleep(2000);
 
-                IWebElement ClickSaveButton = driver.FindElement(By.Id("SaveButton"));
-                ClickSaveButton.Submit();
-                Thread.Sleep(1000);
+            IWebElement ClickSaveButton = driver.FindElement(By.Id("SaveButton"));
+            ClickSaveButton.Submit();
+            Thread.Sleep(1000);
 
-                driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span")).Click();
 
-             }
-            public void DeleteTimeRecord(IWebDriver driver)
-            {
-             //click on Delete button and retrieve the alert message 
+        }
+        public void DeleteTimeRecord(IWebDriver driver)
+        {
+            //click on Delete button and retrieve the alert message 
 
-                IWebElement clickDelete = driver.FindElement(By.XPath("/html/body/div[4]/div/div/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
-                clickDelete.Click();
-                Thread.Sleep(1000);
+            IWebElement clickDelete = driver.FindElement(By.XPath("/html/body/div[4]/div/div/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
+            clickDelete.Click();
+            Thread.Sleep(1000);
 
-                IAlert alert = driver.SwitchTo().Alert();
+            IAlert alert = driver.SwitchTo().Alert();
 
-                string alertBox = alert.Text;
-                Console.WriteLine("Alert box text" + alertBox);
-                alert.Accept();
-
-            //Logout
-
+            string alertBox = alert.Text;
+            Console.WriteLine("Alert box text" + alertBox);
+            alert.Accept();
             driver.Quit();
-            }
-
         }
+
+    }
 }
 
